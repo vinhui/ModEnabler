ea0e09c8bc88eaeb18ac9b0bc73c04eca7976d3c
vinhui <vinni.vinni.vin@gmail.com>
2016-08-30 16:14:16 +0200

    Added animation clip and curve loading

---
+++ ModEnabler/ModEnabler.Resource/ResourceManager.Animation.cs
@@ -0,0 +1,36 @@
+﻿using ModEnabler.Resource.DataObjects;
+using UnityEngine;
+
+namespace ModEnabler.Resource
+{
+    public static partial class ResourceManager
+    {
+        /// <summary>
+        /// Load an animation clip
+        /// </summary>
+        /// <param name="name">Full name and path of the file</param>
+        /// <returns>Returns null if the file doesn't exist or failed to load</returns>
+        public static AnimationClip LoadAnimationClip(string name)
+        {
+            return GetResource(name, "", (bytes) =>
+            {
+                AnimationClipData animationData = ModsManager.serializer.Deserialize<AnimationClipData>(BytesToString(bytes));
+                return animationData.ToUnity();
+            }, "AnimationClip");
+        }
+
+        /// <summary>
+        /// Load an animation curve
+        /// </summary>
+        /// <param name="name">Full name and path of the file</param>
+        /// <returns>Returns null if the file doesn't exist or failed to load</returns>
+        public static AnimationCurve LoadAnimationCurve(string name)
+        {
+            return GetResource(name, "", (bytes) =>
+            {
+                AnimationClipData.AnimationCurveData curveData = ModsManager.serializer.Deserialize<AnimationClipData.AnimationCurveData>(BytesToString(bytes));
+                return curveData.ToUnity();
+            }, "AnimationCurve");
+        }
+    }
+}
