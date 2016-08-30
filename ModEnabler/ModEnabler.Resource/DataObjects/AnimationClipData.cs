1402f5b5d02a44b7b7e5475b63cc0d8edfa58be4
vinhui <vinni.vinni.vin@gmail.com>
2016-08-30 16:12:24 +0200

    Created base for animation clips and animation curves

---
+++ ModEnabler/ModEnabler.Resource/DataObjects/AnimationClipData.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using UnityEngine;
+
+namespace ModEnabler.Resource.DataObjects
+{
+    public struct AnimationClipData
+    {
+        public string name;
+        public bool legacy;
+        public Bounds localBounds;
+        public WrapMode wrapMode;
+        public ClipCurveData[] curves;
+
+        public AnimationClip ToUnity()
+        {
+            AnimationClip clip = new AnimationClip();
+            clip.name = name;
+            clip.legacy = legacy;
+            clip.localBounds = localBounds;
+            clip.wrapMode = wrapMode;
+
+            for (int i = 0; i < curves.Length; i++)
+            {
+                clip.SetCurve(
+                    curves[i].relativePath,
+                    Type.GetType(curves[i].type),
+                    curves[i].propertyName,
+                    curves[i].curve.ToUnity());
+            }
+            return clip;
+        }
+
+        public struct ClipCurveData
+        {
+            public string relativePath;
+            public string type;
+            public string propertyName;
+            public AnimationCurveData curve;
+        }
+
+        public struct AnimationCurveData
+        {
+            public WrapMode postWrapMode;
+            public WrapMode preWrapMode;
+            public Keyframe[] keys;
+
+            public AnimationCurveData(AnimationCurve curve)
+            {
+                postWrapMode = curve.postWrapMode;
+                preWrapMode = curve.preWrapMode;
+                keys = curve.keys;
+            }
+
+            public AnimationCurve ToUnity()
+            {
+                return new AnimationCurve()
+                {
+                    postWrapMode = postWrapMode,
+                    preWrapMode = preWrapMode,
+                    keys = keys
+                };
+            }
+        }
+    }
+}
