eb228ce4d3857d129db07c9d4028ba39bda34545
vinhui <vinni.vinni.vin@gmail.com>
2017-01-10 11:10:38 +0100

    Moved demo stuff to the demo folder

---
+++ Main Unity Project/Assets/Demo/Scripts/LoadSkybox.cs
@@ -0,0 +1,15 @@
+﻿using ModEnabler.Resource;
+using ModEnabler.Resource.Components;
+using UnityEngine;
+
+/// <summary>
+/// Simple script to load a skybox
+/// We use the type Transform here because it doesn't need any componen't to function
+/// </summary>
+public class LoadSkybox : LoadResourceComponent<Transform>
+{
+    public override void Set()
+    {
+        RenderSettings.skybox = ResourceManager.LoadMaterial(base.fileName);
+    }
+}
